48bba06a4c4c330236c3a033f44c6e477b42b9e0
Unknown <19Sebastian95@gmx.de>
2019-01-20 00:32:14 +0100

    iterator for LibUSBDeviceList

--- src/USB/LibUSBDevices.h
+++ src/USB/LibUSBDevices.h
@@ -16,18 +16,26 @@ namespace usb {
 
     class LibUSBDeviceList {
     public:
+        using devices_t = std::unordered_map<size_t, std::shared_ptr<LibUSBDevice>>;
+        using const_iterator = devices_t::const_iterator;
+
         LibUSBDeviceList();
         virtual ~LibUSBDeviceList();
 
         size_t size();
         [[nodiscard]] const std::optional<std::shared_ptr<LibUSBDevice>> findDevice(const VendorID& vendorID, const DeviceID& deviceID) noexcept;
 
+
+        const const_iterator begin() const { return mDevices.begin(); }
+        const const_iterator end() const { return mDevices.end(); }
+        const const_iterator cbegin() const { return mDevices.cbegin(); }
+        const const_iterator cend() const { return mDevices.cend(); }
     private:
         libusb_context *context = nullptr;
         libusb_device **list = nullptr;
         ssize_t device_count = 0;
 
-        std::unordered_map<size_t, std::shared_ptr<LibUSBDevice>> mDevices;
+        devices_t mDevices;
         size_t mDeviceID = 0;
 
         void addDevice(VendorID vendorID, DeviceID deviceID, libusb_device *device);
